54758df3f63cffa164931cc553d01127afd93de6
hsiubb <479833352@qq.com>
2019-04-15 21:26:34 +0800

    Update core.js

--- js/core.js
+++ js/core.js
@@ -15,9 +15,26 @@ $('.item-num-modify').on('click', function() {
 	let $num = $this.siblings('input');
 	let num = parseInt($num.val()) || 0;
 	if($this.hasClass('item-num-minus')) {
-		num = Math.max(num-1, 0);
+		num = Math.max(num-1, 1);
 	} else {
 		num++;
 	}
 	$num.val(num);
+	$num.change();
+});
+
+$('.item-number-input').each(function() {
+	let $this = $(this);
+	let price = parseFloat($this.data('price'));
+	$this.data('unit_total', price);
+	$this.on('change', function() {
+		let num = parseInt($this.val());
+		$this.data('unit_total', num*price);
+		$this.closest('.item-number').next('.item-price').html('￥'+(num*price).toFixed(2));
+		let total = 0;
+		$('.item-number-input').each(function() {
+			total += parseFloat($(this).data('unit_total'));
+		});
+		$('.total-price').html('合 &nbsp; 计 <span>￥'+total.toFixed(2)+'</span>')
+	});
 });
